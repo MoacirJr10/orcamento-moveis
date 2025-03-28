7fbea9cd82b4050850bb805b189c28f2fb9fdfeb
Moacir Pereira <moacir.1000rr@hotmail.com>
2025-03-28 20:04:25 -0300

    Initial commit

---
+++ src/main/java/com/seuprojeto/orcamento/OrcamentoMoveisApplication.java
@@ -0,0 +1,13 @@
+package com.seuprojeto.orcamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoMoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoMoveisApplication.class, args);
+	}
+
+}
