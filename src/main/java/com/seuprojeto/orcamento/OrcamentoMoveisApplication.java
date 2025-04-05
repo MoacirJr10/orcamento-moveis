d192df8a79781e28f416e1ab6a1dd8cf500d5f0b
Moacir Pereira <moacir.1000rr@hotmail.com>
2025-04-04 23:00:11 -0300

    add Application

--- src/main/java/com/seuprojeto/orcamento/OrcamentoMoveisApplication.java
+++ src/main/java/com/seuprojeto/orcamento/OrcamentoMoveisApplication.java
@@ -2,12 +2,31 @@ package com.seuprojeto.orcamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.util.TimeZone;
 
 @SpringBootApplication
 public class OrcamentoMoveisApplication {
+	   private static final Logger logger = LoggerFactory.getLogger(OrcamentoMoveisApplication.class);
+	   public static void main(String[] args){
+		   try{
+			   logger.info("Iniciando aplicação de Orçamento...");
+			   SpringApplication.run(OrcamentoMoveisApplication.class, args);
+			   logger.info("Aplicação iniciada com sucesso.");
+		   } catch (Exception e){
+			   logger.error("Erro ao iniciar a aplicação: ", e);
+			   System.exit(1);
+		   }
+	   }
 
-	public static void main(String[] args) {
-		SpringApplication.run(OrcamentoMoveisApplication.class, args);
-	}
 
+@Postconstruct
+public void init(){
+		   TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+		   logger.info("Fuso horário padrão definido para America/Sao_Paulo.");
+                 }
 }
+
