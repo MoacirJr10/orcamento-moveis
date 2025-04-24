c0149df1aa1cd330f39c74f44e6b07af40e3ac5a
Moacir Pereira <moacir.1000rr@hotmail.com>
2025-04-24 20:03:01 -0300

    Initial commit

---
+++ src/main/java/com/example/orcamento_moveis/OrcamentoMoveisApplication.java
@@ -0,0 +1,13 @@
+package com.example.orcamento_moveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoMoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoMoveisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orcamento_moveis/OrcamentoMoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orcamento_moveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoMoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
